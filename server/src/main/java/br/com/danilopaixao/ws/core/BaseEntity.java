72e7bf5e1fa5803f4ecd91ab49ff03226c82e7f8
danilo.paixao <danilo.paixao@gingaone.com>
2018-08-24 15:41:10 -0300

    rename package, improviments in jpa

--- server/src/main/java/br/com/danilopaixao/ws/core/BaseEntity.java
+++ server/src/main/java/br/com/danilopaixao/ws/core/BaseEntity.java
@@ -1,7 +1,7 @@
-/**package br.com.danilopaixao.ws.core;
+package br.com.danilopaixao.ws.core;
 
 import java.io.Serializable;
-import java.time.LocalDate;
+import java.time.ZonedDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
@@ -9,8 +9,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreRemove;
+import javax.persistence.PreUpdate;
 import javax.persistence.Version;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -36,20 +42,70 @@ public abstract class BaseEntity<I> implements Serializable {
 	private Long version;
 
 	@Column(name = "created_at", nullable = false)
-	private LocalDate createdAt;
+	private ZonedDateTime createdAt;
 
 	@Column(name = "updated_at")
-	private LocalDate updatedAt;
+	private ZonedDateTime updatedAt;
 
 	@Column(name = "removed_at")
-	private LocalDate removedAt;
+	private ZonedDateTime removedAt;
 
 	@CreatedBy
-	@Column(name = "created_by", nullable = false)
+	@Column(name = "created_by")
 	private String createdBy;
 
 	@LastModifiedBy
 	@Column(name = "updated_by")
 	private String updatedBy;
+
+	@PrePersist
+    public void setCreationDate() {
+        this.createdAt = ZonedDateTime.now();
+    }
+
+	@PreUpdate
+    public void setUpdatingDate() {
+        this.updatedAt = ZonedDateTime.now();
+    }
+
+	@PreRemove
+    public void setChangeDate() {
+        this.removedAt = ZonedDateTime.now();
+    }
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null || ! this.getClass().isInstance(obj)) {
+			return false;
+		}
+
+		if (obj == this) {
+			return true;
+		}
+
+		@SuppressWarnings("unchecked")
+		final BaseEntity<I> other = (BaseEntity<I>) obj;
+		
+		if (other.getId() == null) {
+			return false;
+		}
+		
+		return new EqualsBuilder()
+				.append(this.getId(), other.getId())
+				.append(this.getVersion(), other.getVersion())
+				.isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(this.getId()).append(this.getVersion()).append(this.getCreatedAt())
+				.toHashCode();
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this).append(this.getId()).append(this.getVersion()).append(this.getCreatedAt())
+				.toString();
+	}
+
 }
-*/
