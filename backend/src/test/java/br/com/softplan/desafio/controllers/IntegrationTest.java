9bbd7d4b4c5b31741d47f096e5f07e148fa55b6a
Milton J. Neto <milton.neto@infoprice.co>
2018-08-30 02:29:25 -0300

    Changing Readme and excluding ignore on IntegrationTest

--- backend/src/test/java/br/com/softplan/desafio/controllers/IntegrationTest.java
+++ backend/src/test/java/br/com/softplan/desafio/controllers/IntegrationTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,7 +19,6 @@ import java.util.Map;
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 @SpringBootTest(classes = Boot.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Ignore
 public class IntegrationTest {
 
     @Autowired
@@ -34,6 +34,10 @@ public class IntegrationTest {
     protected Map<String, Object> parseResponse(String response) throws IOException {
         return objectMapper.readValue(response, new TypeReference<Map<String, Object>>() {
         });
+    }
+
+    @Test
+    public void empty() {
     }
 
 }
