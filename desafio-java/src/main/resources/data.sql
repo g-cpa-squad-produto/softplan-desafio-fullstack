7e9767bd85725ebbb0eb918b77fa7bda94785ac2
Pablo Lima <pabloguardiao@gmail.com>
2018-09-04 18:52:47 -0300

    Dados de carga para teste

---
+++ desafio-java/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into usuario(nome, matricula, tipo) values ('João', '123', 'ADMININISTRADOR');
+insert into usuario(nome, matricula, tipo) values ('Bia', '456', 'TRIADOR');
+insert into usuario(nome, matricula, tipo) values ('Pablo', '789', 'FINALIZADOR');
+
+insert into processo(numero, dados, pendente) values ('123', 'Processo teste', 'true');
