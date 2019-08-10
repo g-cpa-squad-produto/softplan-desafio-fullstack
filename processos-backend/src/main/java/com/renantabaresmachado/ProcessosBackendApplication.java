8f3e3e2701c966fa64edb08944ed09881601d08c
renantabaresmachado <renantmachado@savef5.com.br>
2019-08-10 13:17:53 -0300

    projeto criado

---
+++ processos-backend/src/main/java/com/renantabaresmachado/ProcessosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.renantabaresmachado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessosBackendApplication.class, args);
+	}
+
+}
