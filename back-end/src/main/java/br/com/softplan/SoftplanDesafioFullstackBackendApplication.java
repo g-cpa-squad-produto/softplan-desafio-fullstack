48f891e51388333d0d72648ee2389e6b0ca0a86e
hebert.ramos <hebert.ramos@azi.srv.br>
2019-09-05 20:47:21 -0400

    Arquitetura inicial da aplicação.

---
+++ back-end/src/main/java/br/com/softplan/SoftplanDesafioFullstackBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.softplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftplanDesafioFullstackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftplanDesafioFullstackBackendApplication.class, args);
+	}
+
+}
