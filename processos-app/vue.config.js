dd7ed5ff6a16f78d9e3115fe545980ffcc2fc6f9
Adolfo Marangoni Hengling <adolfo.hengling@azi.com.br>
2019-09-20 22:18:19 -0400

    - adicionei as configurações para rodar o projeto em desenvolvimento

---
+++ processos-app/vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    devServer: {
+        open: process.platform === 'darwin',
+        host: '0.0.0.0',
+        port: 8080,
+        https: false,
+        hotOnly: false,
+        proxy: {
+            "/api/*": {
+                target: "http://localhost:8081",
+                secure: false
+            }
+        }
+    },
+
+    transpileDependencies: [
+        /\bvue-awesome\b/
+    ]
+}
