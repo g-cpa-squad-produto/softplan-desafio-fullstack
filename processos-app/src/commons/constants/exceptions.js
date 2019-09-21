0e19d5d76f13057d4e2c38fa9901030374e83da1
Adolfo Marangoni Hengling <adolfo.hengling@azi.com.br>
2019-09-21 17:31:46 -0400

    - mapeamento das exceções no frontend para exibir as mensagens quando houver algum erro no processamento

--- processos-app/src/commons/constants/exceptions.js
+++ processos-app/src/commons/constants/exceptions.js
@@ -1,4 +1,9 @@
 export default {
     'default': 'Ocorreu um erro ao realizar a operação.',
+    'br.com.processos.processo.specification.exception.ParecerNaoExistenteException': 'O parecer não foi encontrado.',
+    'br.com.processos.processo.specification.exception.ProcessoNaoExistenteException': 'O processo não foi encontraro.',
+
+    'br.com.processos.usuario.specification.exception.EmailJaCadastradoException': 'O e-mail informado já está sendo utilizado por outro usuário.',
+    'br.com.processos.usuario.specification.exception.UsuarioNaoExisteException': 'O usuário não foi encontrado.',
     'br.com.processos.usuario.specification.exception.UsuarioRelacionadoAProcessosException': 'O usuário não pode ser excluído pois está relacionado a processos.'
 }
