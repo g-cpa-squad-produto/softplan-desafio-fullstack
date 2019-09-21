6ff9cf7011919e242d208284e40f9d5e63732620
Adolfo Marangoni Hengling <adolfo.hengling@azi.com.br>
2019-09-21 15:28:35 -0400

    - listagem de usuários para o administrador

--- processos-app/src/views/pages/usuario/busca/UsuarioBusca.vue
+++ processos-app/src/views/pages/usuario/busca/UsuarioBusca.vue
@@ -1,13 +1,52 @@
 <template>
-    <h1>Usuário Busca</h1>
+    <div>
+        <v-layout class="mb-3">
+            <v-flex xs10>
+                <page-title titulo="Usuários" subtitulo="Listagem dos usuários" rota-voltar="login"/>
+            </v-flex>
+            <v-flex xs2 class="text-xs-right">
+                <v-btn color="green" class="white--text" @click="irParaNovoUsuario">
+                    Novo
+                </v-btn>
+            </v-flex>
+        </v-layout>
+        <usuario-busca-tabela :usuarios="usuarios" @remover="confirmarRemocao" v-if="existeUsuarios"/>
+        <empty-search-results v-else/>
+    </div>
 </template>
 
 <script>
+    import {mapState} from 'vuex'
+    import PageTitle from '@/views/components/PageTitle'
+    import EmptySearchResults from '@/views/components/EmptySearchResults'
+    import UsuarioBuscaTabela from './UsuarioBuscaTabela'
+
     export default {
-        name: 'UsuarioBusca'
+        name: 'UsuarioBusca',
+        components: {PageTitle, EmptySearchResults, UsuarioBuscaTabela},
+        data() {
+            return {
+                confirmandoRemocao: false,
+                usuarioRemocao: null
+            }
+        },
+        computed: {
+            existeUsuarios() {
+                return this.usuarios.length > 0
+            },
+            ...mapState(['usuarios'])
+        },
+        methods: {
+            confirmarRemocao(usuario) {
+                this.usuarioRemocao = usuario
+                this.confirmandoRemocao = true
+            },
+            irParaNovoUsuario() {
+                // TODO
+            },
+            remover() {
+                // TODO
+            }
+        }
     }
 </script>
-
-<style scoped>
-
-</style>
---
+++ processos-app/src/views/pages/usuario/busca/UsuarioBuscaTabela.vue
@@ -0,0 +1,70 @@
+<template>
+    <v-data-table
+            hide-actions
+            class="elevation-1"
+            :headers="headers"
+            :items="usuarios">
+        <template v-slot:items="props">
+            <td>{{ props.item.id }}</td>
+            <td>{{ props.item.nome }}</td>
+            <td>{{ props.item.email }}</td>
+            <td>{{ props.item.tipo | titleCase}}</td>
+            <td class="text-xs-right">
+                <v-tooltip bottom>
+                    <v-btn flat icon slot="activator" @click="remover(props.item)">
+                        <v-icon class="text--secondary">delete</v-icon>
+                    </v-btn>
+                    <span>Remover</span>
+                </v-tooltip>
+            </td>
+        </template>
+    </v-data-table>
+</template>
+
+<script>
+    export default {
+        name: 'UsuarioBuscaTabela',
+        props: ['usuarios'],
+        data() {
+            return {
+                headers: [
+                    {
+                        text: 'Nro.',
+                        align: 'left',
+                        sortable: false,
+                        width: '5%'
+                    },
+                    {
+                        text: 'Nome',
+                        align: 'left',
+                        sortable: false,
+                        width: '30%'
+                    },
+                    {
+                        text: 'E-mail',
+                        align: 'left',
+                        sortable: false,
+                        width: '30%'
+                    },
+                    {
+                        text: 'Papel',
+                        align: 'left',
+                        sortable: false,
+                        width: '10%'
+                    },
+                    {
+                        text: 'Ações',
+                        align: 'right',
+                        sortable: false,
+                        width: '20%'
+                    }
+                ]
+            }
+        },
+        methods: {
+            remover(usuario) {
+                this.$emit('remover', usuario)
+            }
+        }
+    }
+</script>
