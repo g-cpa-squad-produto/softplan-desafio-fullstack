1c0f9e4bce38799a3a04772b3b5956bb7b2d0842
Max Braynner <maxbraynner@gmail.com>
2020-02-04 02:43:28 -0300

    Adição de consulta por e-mail

--- backend/src/main/java/br/com/sofplan/processos/repositories/UsuarioRepository.java
+++ backend/src/main/java/br/com/sofplan/processos/repositories/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package br.com.sofplan.processos.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import br.com.sofplan.processos.models.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+	Optional<Usuario> findByEmail(String email);
+	
+	
 }
