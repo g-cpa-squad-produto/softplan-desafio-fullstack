f477330a38def8ff9b592f6cea043d292a68508c
Max Braynner <maxbraynner@gmail.com>
2020-02-04 02:44:35 -0300

    Carga de usuários

---
+++ backend/src/main/resources/db/migration/V2__Carga_Usuarios.sql
@@ -0,0 +1,14 @@
+-- senha 123456
+INSERT INTO public.tb_usuario (email,senha,perfil,primeiro_nome,sobrenome) VALUES 
+('admin@email.com','$2a$10$SFpiudSEiL994LmH2uV1VuN.m1ACFHQFUXUcrm39dP.2jSnGA6cNG','ADMIN','Admin','admin')
+;
+
+-- senha 123456
+INSERT INTO public.tb_usuario (email,senha,perfil,primeiro_nome,sobrenome) VALUES 
+('triador@email.com','$2a$10$SFpiudSEiL994LmH2uV1VuN.m1ACFHQFUXUcrm39dP.2jSnGA6cNG','TRIADOR','Triador','triador')
+;
+
+-- senha 123456
+INSERT INTO public.tb_usuario (email,senha,perfil,primeiro_nome,sobrenome) VALUES 
+('finalizador@email.com','$2a$10$SFpiudSEiL994LmH2uV1VuN.m1ACFHQFUXUcrm39dP.2jSnGA6cNG','FINALIZADOR','Finalizador','finalizador')
+;
