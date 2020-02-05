dde9b45d24ad7ca013ab9ca18cefdb1b98acdc8a
Max Braynner <maxbraynner@gmail.com>
2020-02-04 23:04:41 -0300

    Adição de consulta findBySituacaoAndResponsavel

--- backend/src/main/java/br/com/sofplan/processos/repositories/ProcessoRepository.java
+++ backend/src/main/java/br/com/sofplan/processos/repositories/ProcessoRepository.java
@@ -1,11 +1,24 @@
 package br.com.sofplan.processos.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import br.com.sofplan.processos.enums.Situacao;
 import br.com.sofplan.processos.models.Processo;
 
 @Repository
 public interface ProcessoRepository extends JpaRepository<Processo, Long> {
 
+	@Query("select processo from Processo processo "
+			+ "join processo.responsaveis responsaveis "
+			+ "left join processo.parecer parecer "
+			+ "where "
+			+ "(parecer.id is null or parecer.situacao = :situacao) and "
+			+ "responsaveis.id.responsavel.id = :usuarioId "
+			+ "order by processo.id asc")
+	List<Processo> findBySituacaoAndResponsavel(Situacao situacao, Long usuarioId);
+	
 }
