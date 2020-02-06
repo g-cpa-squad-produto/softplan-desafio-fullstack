8c547dc54f628be8da0dcbc244fe755163deb8db
Max Braynner <maxbraynner@gmail.com>
2020-02-06 02:34:10 -0300

    Renderização docomponente de rotas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
+import Routes from "./Routes";
+import Header from "./components/Header";
 import Login from "./views/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <Login/>
-    </div>
-  );
+class App extends Component {
+    render() {
+        const { user } = this.props.auth;
+
+        if (!user) {
+            return <Login />;
+        }
+
+        return (
+            <BrowserRouter>
+                <Header />
+                <Routes />
+            </BrowserRouter>
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = state => {
+    return {
+        auth: state.auth
+    };
+};
+
+export default connect(mapStateToProps)(App);
