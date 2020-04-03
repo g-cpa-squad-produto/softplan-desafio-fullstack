ea066d46bdcc97f7046a9716bc3e4fc96da38efe
reginaldo <camilo.melges@gmail.com>
2020-04-03 15:54:16 -0400

    generate api softplan

---
+++ sotfplan-processes/src/main/java/com/example/sotfplanprocesses/SotfplanProcessesApplication.java
@@ -0,0 +1,13 @@
+package com.example.sotfplanprocesses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SotfplanProcessesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SotfplanProcessesApplication.class, args);
+	}
+
+}
---
+++ sotfplan-processes/src/test/java/com/example/sotfplanprocesses/SotfplanProcessesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sotfplanprocesses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SotfplanProcessesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
