46d8be03bd0a7b357f27ef69a6781003815d9784
reginaldo <camilo.melges@gmail.com>
2020-04-04 17:00:11 -0400

    Alter configs for create database schema on start docker

--- processesapi/src/main/java/com/softplan/processesapi/ProcessesapiApplication.java
+++ processesapi/src/main/java/com/softplan/processesapi/ProcessesapiApplication.java
@@ -2,8 +2,10 @@ package com.softplan.processesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class ProcessesapiApplication {
 
 	public static void main(String[] args) {
