b1d8e8819747df42dbe852ece45fa8a76b7f1357
Assis <joaohonorato@gmail.com>
2020-04-06 15:47:03 -0300

    Spring initializer bootstrap

---
+++ process-manager-api/src/main/java/com/softplan/processmanagerapi/ProcessManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.softplan.processmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessManagerApiApplication.class, args);
+	}
+
+}
---
+++ process-manager-api/src/test/java/com/softplan/processmanagerapi/ProcessManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softplan.processmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
