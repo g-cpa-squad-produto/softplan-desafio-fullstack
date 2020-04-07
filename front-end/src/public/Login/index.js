eadbfe9b8b3ca3349c6f6a78fff820a757271031
reginaldo <camilo.melges@gmail.com>
2020-04-06 21:59:07 -0400

    Added preventDefault event on form submitions on login and register

--- front-end/src/public/Login/index.js
+++ front-end/src/public/Login/index.js
@@ -7,14 +7,38 @@ export default class Login extends Component {
   render() {
     return (
       <div className="Login">
-          <form noValidate>
-            <TextField className="input" name="email" label="E-mail" autoComplete="current-email" variant="outlined" />
-            <TextField className="input" id="password" type="password" label="Senha" autoComplete="current-password" variant="outlined"/>
-            <Button className="buttonSave" variant="contained" color="primary">
-              Login
-            </Button>
-          </form>
-          <Link to="/register">Não é cadastrado ainda?</Link>
+        <form
+          noValidate
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log("LOGOU");
+          }}
+        >
+          <TextField
+            className="input"
+            name="email"
+            label="E-mail"
+            autoComplete="current-email"
+            variant="outlined"
+          />
+          <TextField
+            className="input"
+            id="password"
+            type="password"
+            label="Senha"
+            autoComplete="current-password"
+            variant="outlined"
+          />
+          <Button
+            type="submit"
+            className="buttonSave"
+            variant="contained"
+            color="primary"
+          >
+            Login
+          </Button>
+        </form>
+        <Link to="/register">Não é cadastrado ainda?</Link>
       </div>
     );
   }
--- front-end/src/public/Register/index.js
+++ front-end/src/public/Register/index.js
@@ -7,11 +7,40 @@ export default class Register extends Component {
   render() {
     return (
       <div className="Login">
-        <form noValidate>
-          <TextField className="input" name="name" label="Name" variant="outlined" />
-          <TextField className="input" name="email" label="E-mail" autoComplete="current-email" variant="outlined" />
-          <TextField className="input" name="password" type="password" label="Senha" autoComplete="current-password" variant="outlined" />
-          <Button className="buttonSave" variant="contained" color="primary">
+        <form
+          noValidate
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log("Registrou");
+          }}
+        >
+          <TextField
+            className="input"
+            name="name"
+            label="Name"
+            variant="outlined"
+          />
+          <TextField
+            className="input"
+            name="email"
+            label="E-mail"
+            autoComplete="current-email"
+            variant="outlined"
+          />
+          <TextField
+            className="input"
+            name="password"
+            type="password"
+            label="Senha"
+            autoComplete="current-password"
+            variant="outlined"
+          />
+          <Button
+            type="submit"
+            className="buttonSave"
+            variant="contained"
+            color="primary"
+          >
             Cadastrar
           </Button>
         </form>
