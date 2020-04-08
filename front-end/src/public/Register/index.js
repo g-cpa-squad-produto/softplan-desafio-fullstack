7f0751778acfe3b9c90aa29734598cfe5405ad71
reginaldo <camilo.melges@gmail.com>
2020-04-07 23:32:44 -0400

    register user from front-enp app

--- front-end/src/public/Register/index.js
+++ front-end/src/public/Register/index.js
@@ -2,34 +2,61 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { TextField, Button } from "@material-ui/core";
 import "../Login/login.css";
+import AxiosConfig from '../../configs/AxiosConfig';
 
 export default class Register extends Component {
+  state = {
+    user: {
+      name: "",
+      email: "",
+      password: "",
+      type: "ADMIN"
+    },
+  };
+
+  onChange = (e) => {
+    this.setState({ user: { ...this.state.user, [e.target.name]: e.target.value } });
+  };
+
+  register = () => {
+    new AxiosConfig().post("auth/register", this.state.user);
+    console.log(this.state.user);
+  }
+
   render() {
     return (
       <div className="Login">
         <form
-          noValidate
           onSubmit={(e) => {
             e.preventDefault();
-            console.log("Registrou");
+            this.register();
           }}
         >
           <TextField
+            required
             className="input"
             name="name"
+            onChange={(e) => this.onChange(e)}
+            value={this.state.name}
             label="Name"
             variant="outlined"
           />
           <TextField
+            required
             className="input"
             name="email"
+            onChange={(e) => this.onChange(e)}
+            value={this.state.email}
             label="E-mail"
             autoComplete="current-email"
             variant="outlined"
           />
           <TextField
+            required
             className="input"
             name="password"
+            onChange={(e) => this.onChange(e)}
+            value={this.state.password}
             type="password"
             label="Senha"
             autoComplete="current-password"
