51a3f7239002445afe4129be2c6ad0538d3b2758
Assis <joaohonorato@gmail.com>
2020-04-08 10:10:00 -0300

    Infrasctructure update dependecies

--- process-manager-api/src/main/java/com/softplan/processmanagerapi/ProcessManagerApiApplication.java
+++ process-manager-api/src/main/java/com/softplan/processmanagerapi/ProcessManagerApiApplication.java
@@ -2,9 +2,23 @@ package com.softplan.processmanagerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {
+		ProcessManagerApiApplication.class,
+		Jsr310JpaConverters.class
+})
 public class ProcessManagerApiApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProcessManagerApiApplication.class, args);
--- process-manager-api/src/test/java/com/softplan/processmanagerapi/ProcessManagerApiApplicationTests.java
+++ process-manager-api/src/test/java/com/softplan/processmanagerapi/ProcessManagerApiApplicationTests.java
@@ -1,5 +1,6 @@
 package com.softplan.processmanagerapi;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
