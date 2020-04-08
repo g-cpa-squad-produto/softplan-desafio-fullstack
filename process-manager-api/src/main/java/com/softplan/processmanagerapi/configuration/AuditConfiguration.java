ca8f3c48ac72f73c22dda23d4668b0a61a2bd72d
Assis <joaohonorato@gmail.com>
2020-04-08 10:11:45 -0300

    Spring security set up with JWT

---
+++ process-manager-api/src/main/java/com/softplan/processmanagerapi/configuration/AuditConfiguration.java
@@ -0,0 +1,10 @@
+package com.softplan.processmanagerapi.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditConfiguration {
+}
