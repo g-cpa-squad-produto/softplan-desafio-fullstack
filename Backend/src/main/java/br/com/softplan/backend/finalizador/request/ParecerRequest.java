31f9fff67f1ae3f72132c9a44ce72b81eeb75e0a
Felipe Eleoterio <felipe.eleoterio@gmail.com>
2020-04-30 14:17:29 -0300

    documento postman

--- Backend/src/main/java/br/com/softplan/backend/finalizador/request/ParecerRequest.java
+++ Backend/src/main/java/br/com/softplan/backend/finalizador/request/ParecerRequest.java
@@ -3,11 +3,9 @@ package br.com.softplan.backend.finalizador.request;
 import lombok.Data;
 
 import br.com.softplan.backend.finalizador.model.ParecerModel;
-import br.com.softplan.backend.triador.service.ProcessoService;
 
 @Data
 public class ParecerRequest {
-	private final ProcessoService processoService;
 
 	private String usuarioId;
 	private String processoId;
