36912555c46421db5d167238790fdf046f1092f2
ROGER DA SILVA E SILVA <rogersilva84@gmail.com>
2020-11-14 10:44:01 -0300

    Initialized back end

---
+++ process-manager/src/test/java/br/com/rogersilva/processmanager/ProcessManagerApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.rogersilva.processmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProcessManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
