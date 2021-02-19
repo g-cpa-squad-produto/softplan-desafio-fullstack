126b931805910c0026698cf42549b3dd1033d221
Alex Jose <alexjosesilvati@gmail.com>
2021-02-18 22:43:50 -0300

    DEV-alex | melhoramentos

--- back/src/main/java/com/desafio/softplan/SoftplanApplication.java
+++ back/src/main/java/com/desafio/softplan/SoftplanApplication.java
@@ -1,13 +1,10 @@
 package com.desafio.softplan;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class SoftplanApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(SoftplanApplication.class, args);
-    }
+        SpringApplication.run(Initializer.class, args);
 
+    }
 }
